4dc309056dc0dc834f34a2189b6cb963959546f5
Moșneguțu Adrian-Ioan <adrianmosnegutuioan@yahoo.com>
2025-02-06 17:39:06 +0200

    create budget manager
    Created a budget manager class to handle the main logic behind the api

---
+++ src/services/BudgetManager.cjs
@@ -0,0 +1,30 @@
+class BudgetManager {
+    #totalBudget = 0;
+    #envelopes = [];
+
+    getTotalBudget() {
+        return this.#totalBudget;
+    }
+
+    getEnvelopes() {
+        return this.#envelopes;
+    }
+
+    addEnvelope(category, budget) {
+        const newEnvelope = envelopeFactory(category, budget);
+        this.#envelopes.push(newEnvelope);
+        return newEnvelope;
+    }
+}
+
+let envelopeId = 1;
+
+function envelopeFactory(category, budget) {
+    return {
+        id: envelopeId++,
+        category,
+        budget,
+    };
+}
+
+module.exports = BudgetManager;
